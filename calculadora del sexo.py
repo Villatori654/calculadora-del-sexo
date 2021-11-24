1aed46b8dbd53442b0005c152d87290585a2eeaf
villatori <69400864+Villatori654@users.noreply.github.com>
2021-11-24 12:05:44 +0100

    Add files via upload

---
+++ calculadora del sexo.py
@@ -0,0 +1,8 @@
+print("calculadora del sexo")
+print("en caso de no querer sumar mas, poner 0")
+w=int(input("Primer numero: "))
+x=int(input("Segundo numero: "))
+y=int(input("Tercer numero: "))
+z=int(input("Cuarto numero: "))
+#abajo respuesta
+print("aqui esta la respuesta filho da puta: ", w+x+y+z)
